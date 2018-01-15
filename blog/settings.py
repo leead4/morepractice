75a5d1f9b93e5da1935e60cb01854a62992b14af
leead4 <leead4@gmail.com>
2018-01-14 20:38:43 -0600

    sassssss

--- blog/settings.py
+++ blog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-
+from whitenoise import WhiteNoise
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
