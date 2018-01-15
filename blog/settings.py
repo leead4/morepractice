4ab79ea0d300e9888ec34e2a0d784b3a04f82058
leead4 <leead4@gmail.com>
2018-01-14 19:20:24 -0600

    base path

--- blog/settings.py
+++ blog/settings.py
@@ -14,8 +14,7 @@ import os
 import django_heroku
 
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
